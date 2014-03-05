021fe96c0affba97d889497a68e7fbd88c63e19a
1000000000 <lthyde@gmail.com>
2014-03-05 10:22:49 -0500

    Added the option to see LUP decomposition of the sigma game matrix

--- src/main/java/net/clonecomputers/lab/sigmatester/XORSATGenerator.java
+++ src/main/java/net/clonecomputers/lab/sigmatester/XORSATGenerator.java
@@ -1,6 +1,7 @@
 package net.clonecomputers.lab.sigmatester;
 
 import java.awt.Font;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Arrays;
@@ -8,6 +9,7 @@ import java.util.Arrays;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 
+import Jama.LUDecomposition;
 import Jama.Matrix;
 
 public class XORSATGenerator {
@@ -21,22 +23,54 @@ public class XORSATGenerator {
 			String n = JOptionPane.showInputDialog("Enter n");
 			matrix = generateXorSat(Integer.parseInt(k), Integer.parseInt(n));
 		}
-		StringWriter sw = new StringWriter();
-		matrix.print(new PrintWriter(sw), 2, 0);
-		sw.flush();
 		UIManager.put("OptionPane.messageFont", new Font("Monospaced", Font.PLAIN, 12));
-		JOptionPane.showMessageDialog(null, sw);
+		JOptionPane.showMessageDialog(null, toString(matrix));
 		matrix.print(2, 0);
 		System.out.printf("Rank of Matrix: %d\n", matrix.rank());
 		if(JOptionPane.showConfirmDialog(null, "Convert to Sigma Game?", "Continue?", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
 			SigmaGame game = SigmaGameConverter.convertToSigmaGame(matrix);
 			JOptionPane.showMessageDialog(null, game);
 			System.out.println(game);
+			JOptionPane.showMessageDialog(null, "Rank = " + game.getAdjacencyMatrix().rank());
+			System.out.println("Rank = " + game.getAdjacencyMatrix().rank());
 			if(game.getAdjacencyMatrix().rank() == game.getAdjacencyMatrix().getRowDimension() && JOptionPane.showConfirmDialog(null, "Solve?", "Continue?", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
 				String result = Arrays.deepToString(game.solve().toArray());
 				JOptionPane.showMessageDialog(null, result);
 				System.out.println(result);
 			}
+			if(JOptionPane.showConfirmDialog(null, "LUDecomposition?", "Continue?", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+				LUDecomposition lup = new LUDecomposition(game.getAdjacencyMatrix());
+				JOptionPane.showMessageDialog(null, "Matrix is nonsingular? " + lup.isNonsingular());
+				System.out.println("Matrix is nonsingular? " + lup.isNonsingular());
+				JOptionPane.showMessageDialog(null, "L = \n" + toString(lup.getL()));
+				System.out.println("L = \n" + toString(lup.getL()));
+				JOptionPane.showMessageDialog(null, "U = \n" + toString(lup.getU()));
+				System.out.println("U = \n" + toString(lup.getU()));
+				JOptionPane.showMessageDialog(null, "Pivot = " + Arrays.toString(lup.getPivot()));
+				System.out.println("Pivot = " + Arrays.toString(lup.getPivot()));
+				JOptionPane.showMessageDialog(null, "Determinant = " + lup.det());
+				System.out.println("Determinant = " + lup.det());
+				JOptionPane.showMessageDialog(null, "Solution = \n" + toString(lup.solve(game.getDelta())));
+				System.out.println("Solution = \n" + toString(lup.solve(game.getDelta())));
+			}
+		}
+	}
+	
+	public static String toString(Matrix a) {
+		return toString(a, 2, 0);
+	}
+	
+	public static String toString(Matrix a, int w, int d) {
+		try {
+			StringWriter sw = new StringWriter();
+			a.print(new PrintWriter(sw), w, d);
+			sw.flush();
+			String val = sw.toString();
+			sw.close();
+			return val;
+		} catch(IOException e) {
+			e.printStackTrace();
+			return "";
 		}
 	}
 	
