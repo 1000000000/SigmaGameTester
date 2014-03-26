ea045e769e3f78600700cc1251e8d2343bfa8fb8
1000000000 <lthyde@gmail.com>
2014-03-26 14:04:41 -0400

    Added algorithm to find invertible submatrix of the adjacency matrix

--- src/main/java/net/clonecomputers/lab/sigmatester/SigmaGame.java
+++ src/main/java/net/clonecomputers/lab/sigmatester/SigmaGame.java
@@ -46,8 +46,53 @@ public class SigmaGame {
 		if(adjMatrix.rank() == adjMatrix.getRowDimension()) {
 			return Collections.singleton(convertDoubleArrayToBooleanArray(adjMatrix.solve(delta).getColumnVector(0)));
 		} else {
-			throw new RuntimeException("Finish SigmaGame.solve()");
+			Matrix submatrix = findInvertibleSubmatrix(adjMatrix);
+			//TODO Solve
+			throw new RuntimeException("Finish Me!");
 		}
+	}
+	
+	public static Matrix findInvertibleSubmatrix(Matrix bigMatrix) {
+		if(bigMatrix.getColumnDimension() != bigMatrix.getRowDimension()) throw new IllegalArgumentException("Matrix must be square");
+		int size = bigMatrix.getColumnDimension();
+		int rank = bigMatrix.rank();
+		if(rank == bigMatrix.getColumnDimension()) return bigMatrix;
+		int[][] subsets = new int[nChooseK(size, rank)][rank];
+		int max = (int) ((Math.pow(2, rank) - 1) * Math.pow(2, size - rank));
+		int placeInSubsetsArray = 0;
+		for(int testSubset = (int) (Math.pow(2, rank) - 1); testSubset <=  max; ++ testSubset) {
+			int[] testSubsetArray = new int[rank];
+			int digitPlace = 0;
+			int i = 0;
+			while(digitPlace < size && i < testSubsetArray.length) {
+				if((testSubset & (1 << digitPlace)) != 0) {
+					testSubsetArray[i++] = digitPlace;
+				}
+				digitPlace++;
+			}
+			if (digitPlace == size && i == testSubsetArray.length) {
+				subsets[placeInSubsetsArray++] = testSubsetArray;
+			}
+		}
+		for(int[] rowSubset : subsets) {
+			for(int[] colSubset : subsets) {
+				Matrix testMatrix = bigMatrix.getMatrix(rowSubset, colSubset);
+				if(testMatrix.rank() == rank) return testMatrix;
+			}
+		}
+		throw new RuntimeException("Was not able to find matrix: CHECK ME!!!!");
+	}
+	
+	public static int nChooseK(int n, int k) {
+		if(k > n/2) {
+			k = n - k;
+		}
+		if(k < 0) return 0;
+		int nCk = 1;
+		for(int testK = 0; testK <= k; ++testK) {
+				nCk = (nCk * (n - k))/(k + 1);
+		}
+		return k;
 	}
 	
 	public static boolean[] convertDoubleArrayToBooleanArray(double[] array) {
---
+++ src/test/java/net/clonecomputers/lab/sigmatest/SigmaGameTest.java
@@ -0,0 +1,14 @@
+package net.clonecomputers.lab.sigmatest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SigmaGameTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
